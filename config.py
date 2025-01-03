33969bcbfb4597233d1d179819e204899c087685
Sparklingmind46 <sparklingmind42@gmail.com>
2025-01-03 22:27:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ADMINS = int(os.environ.get("ADMINS", "2031106491"))
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://uramit0001:EZ1u5bfKYZ52XeGT@cluster0.qnbzn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "restrictedsaver)
+DB_NAME = os.environ.get("DB_NAME", "restrictedsaver")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
