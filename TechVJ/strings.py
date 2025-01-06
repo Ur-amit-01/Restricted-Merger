d74bdbbfa9a775acfcdeb87fb6dd48145a642202
Sparklingmind46 <sparklingmind42@gmail.com>
2025-01-06 11:53:29 +0530

    Update strings.py

--- TechVJ/strings.py
+++ TechVJ/strings.py
@@ -2,24 +2,21 @@
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
-HELP_TXT = """**🌟 Help Menu**
+HELP_TXT = """**💡 Help Section**
 
-**🔒 Private Chats**
+**1. 🔒 Private Chats**
+➥ Send the invite link (if not already a member).  
+➥ Send the post link to download content.
 
-1. Send the invite link (if not already a member).  
-2. Send the post link to download content.
+**2. 🌐 Public Chats**
+➥ Simply share the post link.
 
-**🌐 Public Chats**
-• Simply share the post link.
+**3. 📂 Batch Mode**
+➥ Download multiple posts using this format:  
+> https://t.me/xxxx/1001-1010
 
-**📂 Batch Mode**
-• Download multiple posts using this format:  
-```
-https://t.me/username/1001-1010
-```
+> **👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : [ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝](https://t.me/Ur_Amit_01)**"""
 
-> **👨‍💻 Developer : @Ur_Amit_01**"""
-       
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
