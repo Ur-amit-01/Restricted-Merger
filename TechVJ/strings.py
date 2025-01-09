a4d598652fe6866eded0bff5bd0e065b058a465e
Ur-amit-01 <heroku467@gmail.com>
2025-01-09 21:32:29 +0530

    Add files via upload

---
+++ TechVJ/strings.py
@@ -0,0 +1,22 @@
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+HELP_TXT = """**💡 Help Section**
+
+**1. 🔒 Private Chats**
+➥ Send the invite link (if not already a member).  
+➥ Send the post link to download content.
+
+**2. 🌐 Public Chats**
+➥ Simply share the post link.
+
+**3. 📂 Batch Mode**
+➥ Download multiple posts using this format:  
+> https://t.me/xxxx/1001-1010
+
+> **👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : [ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝](https://t.me/Ur_Amit_01)**"""
+
+# Don't Remove Credit Tg - @VJ_Botz
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
