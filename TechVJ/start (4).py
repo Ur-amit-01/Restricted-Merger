6e40d34539ea5bbe2c03848fe69f3349b236903c
Ur-amit-01 <heroku467@gmail.com>
2025-01-09 21:29:34 +0530

    Update start (4).py

--- TechVJ/start (4).py
+++ TechVJ/start (4).py
@@ -54,7 +54,6 @@ async def send_start(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
         text=f"<b>👋 Hi {message.from_user.mention}, I am Save Restricted Content Bot. I can send you restricted content by its post link.\n\nKnow how to use bot by - /help</b>\n\n> **👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : [ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝](https://t.me/Ur_Amit_01)**",
-        reply_to_message_id=message.id,
         disable_web_page_preview=True
     )
 
