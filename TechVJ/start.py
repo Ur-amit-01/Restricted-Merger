b3c0eef8b962b5fa9f9ba63d216c587e94e3bdf6
Ur-amit-01 <heroku467@gmail.com>
2025-01-10 03:12:56 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -54,9 +54,9 @@ async def send_start(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
         text=(
-            "<b>✨👋 Hey {mention}, Welcome!</b>\n\n"
-            "<i>I am a powerful bot designed to assist you effortlessly.</i>\n"
-            "<b>• Here’s what I can do for you: 🤖</b>\n\n"
+            "<b>✨👋 Hey {mention}, Welcome!</b>\n"
+            "<i>I am a powerful bot designed to assist you effortlessly.</i>\n\n"
+            ">**• Here’s what I can do for you: 👇🏻🤖**\n\n"
             "📌 <b>Send restricted content by its post link.</b>\n"
             "📌 <b>Merge multiple PDFs into a single file.</b>\n\n"
             "⚙️ <b>Need help ?? use /help</b>\n\n"
