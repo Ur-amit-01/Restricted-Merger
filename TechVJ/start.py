42aa753d0a3d9c206ca1a2ea1058f33d5cffd422
Ur-amit-01 <heroku467@gmail.com>
2025-01-09 12:33:02 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -77,7 +77,7 @@ async def send_cancel(client: Client, message: Message):
     )
 
 
-@Client.on_message(filters.text & filters.private)
+@Client.on_message(filters.text & filters.private & filters.regex("https://t.me/"))
 async def save(client: Client, message: Message):
     if "https://t.me/" in message.text:
         if batch_temp.IS_BATCH.get(message.from_user.id) == False:
