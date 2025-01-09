9f588ada43115e9fdf720f3c4ed7a3d2dd048402
Ur-amit-01 <heroku467@gmail.com>
2025-01-09 20:50:58 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -22,3 +22,19 @@ async def send_help(client: Client, message: Message):
         text=f"{HELP_TXT}",
         disable_web_page_preview=True
     )
+
+
+HELP_TXT = """**💡 Help Section**
+
+**1. 🔒 Private Chats**
+➥ Send the invite link (if not already a member).  
+➥ Send the post link to download content.
+
+**2. 🌐 Public Chats**
+➥ Simply share the post link.
+
+**3. 📂 Batch Mode**
+➥ Download multiple posts using this format:  
+> https://t.me/xxxx/1001-1010
+
+> **👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : [ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝](https://t.me/Ur_Amit_01)**"""
