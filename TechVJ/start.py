1c8261209da63593567c99c950caec9cf435a9b5
Ur-amit-01 <heroku467@gmail.com>
2025-01-21 02:30:22 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -120,18 +120,22 @@ async def about_callback(client: Client, callback_query):
     uptime = get_uptime()  # Get the bot's uptime
     ABOUT_TXT = f"""<b><blockquote>⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟</blockquote>
     
-    ‣ ᴍʏ ɴᴀᴍᴇ : <a href=https://t.me/{callback_query.from_user.username}>{callback_query.from_user.first_name}</a>
-    ‣ ᴍʏ ʙᴇsᴛ ғʀɪᴇɴᴅ : <a href='tg://settings'>ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
-    ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/Ur_Amit_01'>ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝</a> 
-    ‣ ʟɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>ᴘʏʀᴏɢʀᴀᴍ</a> 
-    ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href='https://www.python.org/download/releases/3.0/'>ᴘʏᴛʜᴏɴ 3</a> 
+    ‣ ᴍʏ ɴᴀᴍᴇ : <a href="https://t.me/{callback_query.from_user.username}">{callback_query.from_user.first_name}</a>
+    ‣ ᴍʏ ʙᴇsᴛ ғʀɪᴇɴᴅ : <a href="tg://settings">ᴛʜɪs ᴘᴇʀsᴏɴ</a> 
+    ‣ ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href="https://t.me/Ur_Amit_01">ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝</a> 
+    ‣ ʟɪʙʀᴀʀʏ : <a href="https://docs.pyrogram.org/">ᴘʏʀᴏɢʀᴀᴍ</a> 
+    ‣ ʟᴀɴɢᴜᴀɢᴇ : <a href="https://www.python.org/download/releases/3.0/">ᴘʏᴛʜᴏɴ 3</a> 
     ‣ ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ2.7.1 [sᴛᴀʙʟᴇ]
     ‣ ᴜᴘᴛɪᴍᴇ : {uptime}</b>"""
     
     reply_markup = InlineKeyboardMarkup([
         [InlineKeyboardButton("🔙 Back", callback_data="back")]
     ])
-    await callback_query.message.edit_text(ABOUT_TXT, reply_markup=reply_markup, parse_mode="HTML", disable_web_page_preview=True)
+    
+    try:
+        await callback_query.message.edit_text(ABOUT_TXT, reply_markup=reply_markup, parse_mode="HTML", disable_web_page_preview=True)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 @Client.on_callback_query(filters.regex("help"))
