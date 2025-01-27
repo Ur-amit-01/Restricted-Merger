3c3ad36ff97385f4926c78c6247f13a1133bfe89
Ur-amit-01 <heroku467@gmail.com>
2025-01-27 12:52:08 +0530

    Update callbacks.py

--- Amit/callbacks.py
+++ Amit/callbacks.py
@@ -12,6 +12,22 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+HELP_TXT = """**💡 Help Section**
+
+**1. 🔒 Private Chats**
+➥ Send the invite link (if not already a member).  
+➥ Send the post link to download content.
+
+**2. 🌐 Public Chats**
+➥ Simply share the post link.
+
+**3. 📂 Batch Mode**
+➥ Download multiple posts using this format:  
+> https://t.me/xxxx/1001-1010
+
+> **👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : [ꫝᴍɪᴛ ꢺɪɴɢʜ ⚝](https://t.me/Ur_Amit_01)**"""
+
+#------------------- MERGE -------------------#
 
 MERGE_TXT = """**⚙️ Hᴇʟᴘ Dᴇsᴄʀɪᴘᴛɪᴏɴ ⚙️**
 
