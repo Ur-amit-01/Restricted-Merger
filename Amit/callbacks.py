ba59e12539be5b33dc0d6588c38abcd18587f541
Amit <heroku467@gmail.com>
2025-02-20 01:28:12 +0530

    Update callbacks.py

--- Amit/callbacks.py
+++ Amit/callbacks.py
@@ -22,7 +22,7 @@ random_images = [
 ]
 
 @Client.on_message(filters.command("start") & filters.private)
-@Client.on_callback_query(filters.regex("uptime"))
+@Client.on_callback_query(filters.regex("start"))
 async def account_login(client: Client, message: Message):
     random_image = random.choice(random_images)
 
