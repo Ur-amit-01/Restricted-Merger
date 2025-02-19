2990f8b7ca64dc31b33bf9189e08e8bb5ad2b507
Amit <heroku467@gmail.com>
2025-02-19 13:46:21 +0530

    Update restricted.py

--- Amit/restricted.py
+++ Amit/restricted.py
@@ -8,24 +8,10 @@ from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 from config import API_ID, API_HASH, BOT_TOKEN, ERROR_MESSAGE, SESSION_STRING
-from Amit.callbacks import about_callback, help_callback
 
 start_time = time.time()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-    
-#@Client.on_message(filters.command("start"))
-#async def send_start(client: Client, message: Message):
- #   logger.info(f"/start command triggered by {message.from_user.id}")  # Log the start command
-  #  start_text = (
-   #     f"> **✨👋🏻 Hey {message.from_user.mention} !!**\n\n"
-    #    "**🔋 ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ʙᴏᴛ ᴅᴇꜱɪɢɴᴇᴅ ᴛᴏ ᴀꜱꜱɪꜱᴛ ʏᴏᴜ ᴇꜰꜰᴏʀᴛʟᴇꜱꜱʟʏ.**\n\n"
-  #      "**🔘 Usᴇ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ ᴛᴏ ʟᴇᴀʀɴ ᴍᴏʀᴇ ᴀʙᴏᴜᴛ ᴍʏ ғᴜɴᴄᴛɪᴏɴs!**"
- #   )
-  #  reply_markup = InlineKeyboardMarkup([
-  #      [InlineKeyboardButton("💡 About", callback_data="about"), InlineKeyboardButton("📖 Help", callback_data="help")]
-  #  ])
-#    await message.reply_text(start_text, reply_markup=reply_markup)
     
 #————————————————————————————————————————————————————————————————————————————————————————————
 
