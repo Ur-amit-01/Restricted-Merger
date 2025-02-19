cf332f6faa4d401c63a31e5a5e9cc034ae47bbf4
Amit <heroku467@gmail.com>
2025-02-20 02:00:40 +0530

    Update callbacks.py

--- Amit/callbacks.py
+++ Amit/callbacks.py
@@ -4,6 +4,10 @@ import random
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, BotCommand
 from config import BOT_TOKEN
+import os
+import requests
+import asyncio
+
 
 # Define the bot's start time
 START_TIME = time.time()
@@ -112,3 +116,38 @@ async def stickerid(bot, message):
 
 #jchfvhfuhcudchuecuegdchuehuchuehccuehucehuc
 
+def upload_image_requests(image_path):
+    upload_url = "https://envs.sh"
+
+    try:
+        with open(image_path, 'rb') as file:
+            files = {'file': file} 
+            response = requests.post(upload_url, files=files)
+
+            if response.status_code == 200:
+                return response.text.strip() 
+            else:
+                return print(f"Upload failed with status code {response.status_code}")
+
+    except Exception as e:
+        print(f"Error during upload: {e}")
+        return None
+
+@Client.on_message(filters.command("telegraph"))
+async def telegraph_upload(bot, update):
+    t_msg = await bot.ask(chat_id = update.from_user.id, text="📸 **Now Send Me Your Photo Or Video Under 5MB To Get Media Link** 🎥")
+    if not t_msg.media:
+        return await update.reply_text("❌ **Only Media Supported.** 📲")
+    path = await t_msg.download()
+    uploading_message = await update.reply_text("<b>⏳ ᴜᴘʟᴏᴀᴅɪɴɢ...</b>")
+    try:
+        image_url = upload_image_requests(path)
+        if not image_url:
+            return await uploading_message.edit_text("❌ **Failed to upload file.**")
+    except Exception as error:
+        await uploading_message.edit_text(f"❌ **Upload failed: {error}**")
+        return
+    await uploading_message.edit_text(
+        text=f"<b>🔗 **Link** :-\n{image_url}</b>",
+        disable_web_page_preview=True
+    )
