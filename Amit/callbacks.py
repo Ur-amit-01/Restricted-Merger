f215e4a36201ec1e37e56b5019c9ae3b81e0678a
Amit <heroku467@gmail.com>
2025-02-20 02:01:38 +0530

    Update callbacks.py

--- Amit/callbacks.py
+++ Amit/callbacks.py
@@ -7,7 +7,11 @@ from config import BOT_TOKEN
 import os
 import requests
 import asyncio
-
+import traceback
+from asyncio import get_running_loop
+from io import BytesIO
+from googletrans import Translator
+from gtts import gTTS
 
 # Define the bot's start time
 START_TIME = time.time()
@@ -151,3 +155,36 @@ async def telegraph_upload(bot, update):
         text=f"<b>🔗 **Link** :-\n{image_url}</b>",
         disable_web_page_preview=True
     )
+
+#jvgfuchjdchjehcjehjehjhcjehcjehhejchejejf
+
+def convert(text):
+    audio = BytesIO()
+    i = Translator().translate(text, dest="en")
+    lang = i.src
+    tts = gTTS(text, lang=lang)
+    audio.name = lang + ".mp3"
+    tts.write_to_fp(audio)
+    return audio
+
+
+@Client.on_message(filters.command("tts"))
+async def text_to_speech(bot, message: Message):
+    vj = await bot.ask(chat_id = message.from_user.id, text = "Now send me your text.")
+    if vj.text:
+        m = await vj.reply_text("Processing")
+        text = vj.text
+        try:
+            loop = get_running_loop()
+            audio = await loop.run_in_executor(None, convert, text)
+            await vj.reply_audio(audio)
+            await m.delete()
+            audio.close()
+        except Exception as e:
+            await m.edit(e)
+            e = traceback.format_exc()
+            print(e)
+    else:
+        await vj.reply_text("Send me only text Buddy.")
+
+
