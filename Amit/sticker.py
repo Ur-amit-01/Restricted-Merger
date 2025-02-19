b02ebe5ebfa5df57bc53e04fafc521b33ef2f3fa
Amit <heroku467@gmail.com>
2025-02-20 01:58:58 +0530

    Delete Amit/sticker.py

--- Amit/sticker.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("stickerid") & filters.private)
-async def stickerid(bot, message):
-    s_msg = await bot.ask(chat_id=message.from_user.id, text="🌟 Now Send Me Your Sticker 📲")
-    if s_msg.sticker:
-        await s_msg.reply_text(f"> **Sticker ID is** ✨ \n `{s_msg.sticker.file_id}` \n \n> **Unique ID is** 🔑 \n\n`{s_msg.sticker.file_unique_id}`")
-    else: 
-        await s_msg.reply_text("Oops !! ❌ Not a sticker file 😕")
